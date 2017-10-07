772327e11f05f197a6a8be18df6eba7fa8c9b6e7
thedreamsaver <ashishjhaofficial@outlook.com>
2017-10-08 02:29:15 +0530

    All set for presentation

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |d|
+  FreelanceDocument.create!(
+      title: "Document #{d}",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      file_url: "https://www.google.com",
+      image_url: "https://static.pexels.com/photos/56759/pexels-photo-56759.jpeg"
+  )
+end
